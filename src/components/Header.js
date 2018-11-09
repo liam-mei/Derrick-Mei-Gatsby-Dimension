e334a956797e1759ee0251cc9d0b93d09a04a7cf
derrick-mei <derrick.mei.168@gmail.com>
2018-11-08 21:38:23 -0600

    changing homepage to include resume button

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,19 +13,20 @@ const Header = (props) => (
                 <h1>Derrick Mei</h1>
                 {/* <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
                 for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p> */}
-                <ul className="icons">
+                <p>Coffee, Code, and C</p>
+                {/* <ul className="icons">
                     <li><a href={resume} class="icon fa-file-pdf-o"><span class="label">Resume</span></a></li>
-                    {/* <li><a href="/resume/" class="icon fa-file-pdf-o"><span class="label">Resume</span></a></li> */}
+                    <li><a href="/resume/" class="icon fa-file-pdf-o"><span class="label">Resume</span></a></li>
                     <li><a href="https://github.com/Derrick-Mei/" target="_blank" rel="noopener noreferrer" className="icon fa-github"><span className="label">GitHub</span></a></li>
                     <li><a href="https://www.linkedin.com/in/derrick-mei/" target="_blank" rel="noopener noreferrer" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-                </ul>
+                </ul> */}
             </div>
         </div>
         <nav>
             <ul>
                 <li><a href="#no" onClick={() => {props.onOpenArticle('projects')}}> Projects </a></li>
                 <li><a className="about" href="#no" onClick={() => {props.onOpenArticle('about')}}> About</a></li>
-                <li><a href="#no" onClick={() => {props.onOpenArticle('experience')}}>Experience</a></li>
+                <li><a className="resume" href="#no" onClick={() => {props.onOpenArticle('resume')}}>Resume</a></li>
                 <li><a href="#no" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 //Falcano Pic
 import falcano from '../images/falcano.svg'
@@ -10,42 +12,118 @@ import resume from '../images/resume.pdf'
 
 class Main extends React.Component {
   render() {
-
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      />
+    )
 
     return (
-      <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
-        <article id="projects" className={`${this.props.article === 'projects' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+      <div
+        ref={this.props.setWrapperRef}
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="projects"
+          className={`${this.props.article === 'projects' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Projects</h2>
-          <span className="image main"><img src={falcano} alt="" /></span>
+          <span className="image main">
+            <img src={falcano} alt="" />
+          </span>
           <div>
-					<h3>Falcano</h3>
-					<p>An app to log your required flight experience to qualify for a pilot license. Users create aircraft and instructors and then
-            log flights using that data. Flight logs are broken down into 12 different fields needed to be completed to qualify for the license. 
-            The 12 different fields are aggregated by airplane flown, airplane license type, and overall total. 
-            Stripe payments were used to gate premium features while Chart.js was used to beautifully display the data as a premium feature.
-            This was a group project in which I focused on the back-end with a focus on Stripe Payments.</p>
-					<span className="bold">Tech Stack:</span><span> React, PostgreSQL, Django</span>
-					<div className="flexSpaceAround"><button><a href='https://falcano.app'>View Project</a></button><button className="githubButton"><a href='https://github.com/Lambda-School-Labs/CS10-flight-log'>Github</a></button></div>
-				</div>
-				<br />
+            <h3>Falcano</h3>
+            <p>
+              An app to log your required flight experience to qualify for a
+              pilot license. Users create aircraft and instructors and then log
+              flights using that data. Flight logs are broken down into 12
+              different fields needed to be completed to qualify for the
+              license. The 12 different fields are aggregated by airplane flown,
+              airplane license type, and overall total. Stripe payments were
+              used to gate premium features while Chart.js was used to
+              beautifully display the data as a premium feature. This was a
+              group project in which I focused on the back-end with a focus on
+              Stripe Payments.
+            </p>
+            <span className="bold">Tech Stack:</span>
+            <span> React, PostgreSQL, Django</span>
+            <div className="flexSpaceAround">
+              <button>
+                <a href="https://falcano.app">View Project</a>
+              </button>
+              <button className="githubButton">
+                <a href="https://github.com/Lambda-School-Labs/CS10-flight-log">
+                  Github
+                </a>
+              </button>
+            </div>
+          </div>
+          <br />
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="about"
+          className={`${this.props.article === 'about' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">About</h2>
-          <p>Communictor, Teamplayer & JavaScript enthusiast, my main technical strengths are focused around User interface and User Experience leveraging the power of React, Redux, 
-            Bootstrap and traditional CSS to create truly unique user experiences. I have deployed back-end projects and APIs leveraging Node, Express, MongoDB, MySQL, and Sqlite3. 
-            I hold a special place in my heart for the biological field, and financial industries and 
-            strongly believe that combined with Computer Science they can solve real world problems.</p>
+          <p>
+            Communictor, Teamplayer & JavaScript enthusiast, my main technical
+            strengths are focused around User interface and User Experience
+            leveraging the power of React, Redux, Bootstrap and traditional CSS
+            to create truly unique user experiences. I have deployed back-end
+            projects and APIs leveraging Node, Express, MongoDB, MySQL, and
+            Sqlite3. I hold a special place in my heart for the biological
+            field, and financial industries and strongly believe that combined
+            with Computer Science they can solve real world problems.
+          </p>
+          <ul className="icons">
+            {/* <li><a href={resume} class="icon fa-file-pdf-o"><span class="label">Resume</span></a></li>
+                    <li><a href="/resume/" class="icon fa-file-pdf-o"><span class="label">Resume</span></a></li> */}
+            <li>
+              <a
+                href="https://github.com/Derrick-Mei/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/derrick-mei/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="icon fa-linkedin"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
+          </ul>
           <img className="profilePic" src={profilePic} alt="" />
           {close}
         </article>
 
-        <article id="experience" className={`${this.props.article === 'experience' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Experience</h2>
-          <span className="image main"><img className="lambdalogo" src={LambdaLogo} alt="" /></span>
+        <article
+          id="resume"
+          className={`${this.props.article === 'resume' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Resume</h2>
+          {/* <span className="image main"><img className="lambdalogo" src={LambdaLogo} alt="" /></span>
           <p>Proficiency with JavaScript, HTML, CSS, LESS, SaSS, React/Redux, Bootstrap, GatsbyJS, Material UI, GSAP, NodeJS, Express,
 					and many more!</p>
 				<h3>Full Stack Web Developer</h3>
@@ -56,13 +134,57 @@ class Main extends React.Component {
 					<li>Gained experience working with clients and understanding client demands by collaborating on full life cycle
 						projects</li>
 					<li>Hands on experience with client and server testing, paired programming, and CS fundamentals</li>
-				</ul>
+        </ul> */}
+          <div className="iframe-container">
+            <iframe
+              src="https://resume.creddle.io/embed/6agspcn5hmv"
+              width="850"
+              height="1100"
+              title="resume"
+              seamless
+            />
+          </div>
+          {/* <Layout> */}
+          <object
+            data={resume}
+            width="850"
+            height="1130"
+            type="application/pdf"
+          >
+            <iframe title="resume" src={resume} scrolling="no" seamless>
+              This browser does not support PDFs. Please download the PDF to
+              view it:
+              <a href={resume} download="">
+                Download PDF
+              </a>
+            </iframe>
+          </object>
+          {/* </Layout> */}
+          <div>
+            {/* <Link className="button" to="/">
+      Home
+    </Link> */}
+
+            <a className="button" href={resume} download="DerrickMei">
+              <i class="fa fa-download" />
+              Download
+            </a>
+          </div>
           {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Contact</h2>
-          <form method="post" action="https://formspree.io/derrick.mei.168@gmail.com">
+          <form
+            method="post"
+            action="https://formspree.io/derrick.mei.168@gmail.com"
+          >
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
@@ -73,21 +195,46 @@ class Main extends React.Component {
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" id="message" rows="4" />
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
             </ul>
           </form>
           <ul className="icons">
-            <li><a href={resume} class="icon fa-file-pdf-o"><span class="label">Resume</span></a></li>
-            <li><a href="https://github.com/Derrick-Mei/" target="_blank" rel="noopener noreferrer" className="icon fa-github"><span className="label">GitHub</span></a></li>
-            <li><a href="https://www.linkedin.com/in/derrick-mei" target="_blank" rel="noopener noreferrer" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+            <li>
+              <a href={resume} class="icon fa-file-pdf-o">
+                <span class="label">Resume</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/Derrick-Mei/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/derrick-mei"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="icon fa-linkedin"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
           </ul>
           {close}
         </article>
-
       </div>
     )
   }
